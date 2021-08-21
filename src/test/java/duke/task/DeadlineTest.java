5a16c5f3b7d4f99358ed2a765b685c68426e14de
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-08-21 10:06:51 +0800

    Add test for deadline class

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -9,46 +9,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class EventTest {
+public class DeadlineTest {
     @Test
     public void testStringConversion() throws InvalidDateException {
         assertEquals(
-                "[E][ ] event 1 (at: Oct 10 2020)",
-                new Event("event 1", Date.of("2020-10-10")).toString());
+                "[D][ ] deadline 1 (by: Oct 10 2020)",
+                new Deadline("deadline 1", Date.of("2020-10-10")).toString());
     }
 
     @Test
     public void testInvalidDate() {
-        assertThrows(InvalidDateException.class, () -> new Event("event 1", Date.of("10-10-2020")));
+        assertThrows(InvalidDateException.class, () -> new Deadline("deadline 1", Date.of("10-10-2020")));
     }
 
     @Test
     @SuppressWarnings("unchecked")
     public void testJSONConversion() throws BadInputFormatException, InvalidDateException {
         JSONObject object = new JSONObject();
-        object.put("type", "event");
+        object.put("type", "deadline");
         object.put("isDone", false);
-        object.put("description", "event 1");
+        object.put("description", "deadline 1");
         object.put("date", "2020-10-10");
-        assertEquals(object.toJSONString(), new Event("event 1 /at 2020-10-10").toJSONObject().toJSONString());
+        assertEquals(
+                object.toJSONString(),
+                new Deadline("deadline 1 /by 2020-10-10").toJSONObject().toJSONString());
     }
 
     @Test
     public void testInvalidInputFormat() {
-        assertThrows(BadInputFormatException.class, () -> new Event("event 1"));
+        assertThrows(BadInputFormatException.class, () -> new Deadline("deadline 1"));
     }
 
     @Test
     public void testFactoryMethod1() throws BadInputFormatException, InvalidDateException {
         assertEquals(
-                new Event("event 1 /at 2020-10-10").toString(),
-                Event.of("event 1 /at 2020-10-10").toString());
+                new Deadline("deadline 1 /by 2020-10-10").toString(),
+                Deadline.of("deadline 1 /by 2020-10-10").toString());
     }
 
     @Test
     public void testFactoryMethod2() throws InvalidDateException, BadInputFormatException {
         assertEquals(
-                new Event("event 1 /at 2020-10-10").toString(),
-                Event.of("event 1", Date.of("2020-10-10"), false).toString());
+                new Deadline("deadline 1 /by 2020-10-10").toString(),
+                Deadline.of("deadline 1", Date.of("2020-10-10"), false).toString());
     }
 }
